e5c36ec429837d340da9bfb679cb5ff29e275648
Frederic Laffont <frederic_laffont@msn.com>
2021-09-24 21:07:32 +0200

    change template

--- template.rb
+++ template.rb
@@ -202,6 +202,6 @@ after_bundle do
 
   # Git
   ########################################
-  git add: '.'
-  git commit: "-m 'Initial commit with devise'"
+  # git add: '.'
+  # git commit: "-m 'Initial commit with devise'"
 end
